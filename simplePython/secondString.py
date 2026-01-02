3dd5f6f3b6dd3983ed82c227acba62274b3ec8a7
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-01-03 01:37:50 +0530

    add first and second string and slicing conditional topics

--- simplePython/secondString.py
+++ simplePython/secondString.py
@@ -43,3 +43,39 @@ elif(light=="yellow"):
     print("wait")
     
 print("end of code")
+
+
+# marks exmaple with grade example
+
+# marks=int(input("enter your marks"))
+
+# if(marks>=90):
+#     print("grade A")
+# elif(marks>=80 and marks<90):
+#     print("grade B")
+# elif(marks>=70 and marks<80):
+#     print("grade c")
+# elif(marks>=60 and marks<70):
+#     print("grade d")
+# elif(marks>=50 and marks<60):
+#     print("grade e")
+    
+# else:
+#     print("fail")
+    
+    
+    
+    # nesting loops
+age=int(input("enter your age"))
+
+if(age>=18):
+    if(age>=80):
+        print("you can vote and also you are senior citizen")
+    else:
+        print("you can vote")
+else:
+    print("you cannot vote")
+    
+    
+
+# practise question with odd and even number
