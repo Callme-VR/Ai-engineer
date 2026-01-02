bc6670c233633cf30a05e9f6db9f3f27266a3506
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-01-03 01:23:38 +0530

    add

---
+++ simplePython/first.py
@@ -0,0 +1,86 @@
+# from ast import operator
+# print("hello world")
+
+# print("my name is Vishal Rajput")
+
+# # if we have to seprate the line then we have to use the \n and print(function ) for the new lines
+# print(34)
+# name="vishal"
+# age=21
+# price=23.44
+# print(name,age,price)
+
+
+# print(type(age))
+
+# print(type(name))
+# print(type(price))
+
+
+# a=2
+# b=3
+# sum=a+b
+# print(sum)
+
+# A=8
+# B=3
+
+# print(A==B)
+# print(A!=B)
+# print(A>B)  
+# print(A<B)
+# print(A>=B)
+# print(A<=B)
+
+# # logical operator
+
+
+# print(not False)
+
+# val1=True
+# val2=False
+
+# print(val1 and val2)
+# print(val1 or val2)
+
+
+
+# # types of conversion
+# # 1. implicit conversion 
+# #  conoverting one varibale into naother variable types is called type conversion
+
+# # typecasting -forcefully converting one varibale into another variable types
+
+# # float have suprieor value as compare to other value
+
+# a=2
+# b=4.5
+# sum=a+b
+# print(sum)
+# print(type(sum))
+
+# # for type casting exmaple
+
+# a=int("2")
+# b=4
+# sum=a+b
+# print(sum)
+# print(type(sum))
+
+
+
+
+# input statement 
+
+
+# name=input("Enter your name:")
+# print("welcome:",name)
+
+
+name=input("Enter your name:")
+age=int(input("Enter your Age:"))
+marks=float(input("Enter your marks:"))
+
+print("name:",name)
+print("age:",age)
+print("marks:",marks)
---
+++ simplePython/secondString.py
@@ -0,0 +1,45 @@
+str1="this is the first string"
+str2='this is the second string'
+
+finalstr=str1+"\n" +str2
+print (finalstr)
+
+
+print(len(finalstr))
+print(finalstr[3])
+
+str="vishal"
+print(str[1:4])
+
+
+
+print(str.endswith("l"))
+print(str.capitalize())
+print(str.find("v"))
+print(str.replace("vishal","vishnu"))
+print(str.count("v"))
+
+
+# conditional statements
+
+# Age=18
+
+# if(Age>=18):
+#     print("Y C Apply here!")
+# elif(Age==18):
+#     print("C Apply here!")
+# else:
+#     print("No you canot Apply here!")
+
+
+
+
+light="yellow"
+if(light=="red"):
+    print("stop")
+elif(light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("wait")
+    
+print("end of code")
