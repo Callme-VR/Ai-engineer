2d5a5630019aa5fce11e9de4fdc59ab11cc8e5c4
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-01-03 23:46:44 +0530

    while loop in python and for loops

--- simplePython/DictionaryandTuple.py
+++ simplePython/DictionaryandTuple.py
@@ -3,4 +3,14 @@ dict={
     "age":23,
     "gender":"Male"
 }
+# print(dict)
+print(dict.keys())
+
+dict.update({"city":"Delhi"})
 print(dict)
+
+values={
+    ("float",9.9),("int",9)
+    
+}
+print(values)
---
+++ simplePython/LoopsinPython.py
@@ -0,0 +1,12 @@
+# count=1
+# while count<=6:
+#     print("count")
+#     count=count+1
+# print("while loop ended")
+
+i=5
+while i>=1:
+    print(i)
+    i-=1
+    
+print("while loop ended")
